382c2730514564edbce7d58c37fbe86f2bcf1067
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-25 17:10:33 +0100

    Creates views dir and index.erb

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
+
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
   end
 
   run! if app_file == $0
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Entering Players' do
+  scenario 'User can enter names for players' do
+    visit('/')
+    fill_in :player_1_name, with: 'Picard'
+    fill_in :player_2_name, with: 'Borg'
+    click_button 'Submit'
+    expect(page).to have_content 'Picard vs. Borg'
+  end
+end
