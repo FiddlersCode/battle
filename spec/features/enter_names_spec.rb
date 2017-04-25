d9379b49bcdcb73c3335386c2c45ae1f62ac39c3
Paula <pmuldoon@umich.edu>
2017-04-25 17:34:07 +0100

    added play.erb

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,9 @@
 feature 'Entering Players' do
   scenario 'User can enter names for players' do
     visit('/')
-    fill_in :player_1_name, with: 'Picard'
-    fill_in :player_2_name, with: 'Borg'
+    fill_in :player_1_name, with: "Picard"
+    fill_in :player_2_name, with: "Borg"
     click_button 'Submit'
-    expect(page).to have_content 'Picard vs. Borg'
+    expect(page).to have_content "Picard vs. Borg"
   end
 end
