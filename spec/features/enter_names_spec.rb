c704a9d213e5cbaa3199f81b7fa60e5924d3078b
Paula <pmuldoon@umich.edu>
2017-04-26 12:34:40 +0100

    added web_helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,7 @@
 feature 'Entering Players' do
   scenario 'User can enter names for players' do
     visit('/')
-    fill_in :player_1_name, with: 'Picard'
-    fill_in :player_2_name, with: 'Borg'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Picard vs. Borg'
   end
 end
