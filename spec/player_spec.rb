3d2a38e3ab054c1f799e3090399889bbd34d2dc0
Paula <pmuldoon@umich.edu>
2017-04-26 15:12:52 +0100

    fixed the localhost require ./lib/player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,23 +8,4 @@ describe Player do
       expect(picard.name). to eq 'Picard'
     end
   end
-
-  describe '#laser_strikes' do
-    it 'returns the laser strikes' do
-      expect(dave.laser_strikes).to eq described_class::DEFAULT_LASER_STRIKES
-    end
-  end
-
-  describe '#attack' do
-    it 'damages the player' do
-      expect(borg).to recieve(:receive_damage)
-      picard.attack(borg)
-    end
-  end
-
-  describe '#receive_damage' do
-    it 'reduces the player laser strikes' do
-      expect { picard.receive_damage }.to change { picard.laser_strikes}.by(-1)
-    end
-  end
 end
