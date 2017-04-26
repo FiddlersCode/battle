dd4aadf0e561f25bd229bbe907dc7a6d2d82f151
Paula <pmuldoon@umich.edu>
2017-04-26 15:34:46 +0100

    player rspec tests green

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,17 @@
 class Player
-  attr_reader :name
+  DEFAULT_LASER_STRIKES = 10
+  attr_reader :name, :laser_strikes
 
-  def initialize(name)
+  def initialize(name, laser_strikes = DEFAULT_LASER_STRIKES)
     @name = name
+    @laser_strikes = laser_strikes
+  end
+
+  def attack(enemy)
+    enemy.sustain_damage
+  end
+
+  def sustain_damage
+    @laser_strikes -= 1
   end
 end
